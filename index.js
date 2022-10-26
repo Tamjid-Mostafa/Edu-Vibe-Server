b11e00da1250d9015f3d37d413f5973584ae2600
Tamjid-Mostafa <tamjid430@gmail.com>
2022-10-26 19:23:10 +0600

    added index.js

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+const cors = require('cors');
+const course = require('./data/course.json');
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('Course api running');
+});
+
+app.get('/courses', (req, res) => {
+    res.send(course)
+});
+
+
+
+app.get('/courses/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedCourse = course.find(n => n.id === id);
+    res.send(selectedCourse);
+});
+
+app.listen(port, () => {
+    console.log('Course list', port);
+})
